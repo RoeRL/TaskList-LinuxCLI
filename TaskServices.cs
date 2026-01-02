9d707bb6682eefc918c6093f5e66e1561cef3415
RoeRL <bimo.17168@gmail.com>
2026-01-02 17:30:43 +0700

    [RR]: Fix Naming

--- TaskServices.cs
+++ TaskServices.cs
@@ -8,34 +8,35 @@ namespace TaskList.Services
 {
     public class TaskService
     {
-        private static string m_homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        private static string m_folderPath = Path.Combine(m_homePath, ".config", "tasklist");
-        private static string m_filePath = Path.Combine(m_folderPath, "task.json");
+        private static string _mHomePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        private static string _mFolderPath = Path.Combine(_mHomePath, ".config", "tasklist");
+        private static string _mFilePath = Path.Combine(_mFolderPath, "task.json");
+        private string _defaultJsonContent = "{}";
         
         public void AddTask()
         {
-            string defaultJsonContent = "{}";
-            if (!Directory.Exists(m_folderPath)) Directory.CreateDirectory(m_folderPath);
-            if (!File.Exists(m_filePath)) File.WriteAllText(m_filePath, defaultJsonContent);
+            
+            if (!Directory.Exists(_mFolderPath)) Directory.CreateDirectory(_mFolderPath);
+            if (!File.Exists(_mFilePath)) File.WriteAllText(_mFilePath, _defaultJsonContent);
             Guid uniqueId = Guid.NewGuid();
             string newUid = uniqueId.ToString();
             Console.WriteLine("Enter the Task Title: ");
-            string m_title = Console.ReadLine();
-            string old_jsonString = File.ReadAllText(m_filePath);
-            string formattedJsonString = JValue.Parse(old_jsonString).ToString(Formatting.Indented);
+            string mTitle = Console.ReadLine();
+            string oldJsonString = File.ReadAllText(_mFilePath);
+            string formattedJsonString = JValue.Parse(oldJsonString).ToString(Formatting.Indented);
             TaskModel taskModel = new TaskModel
             {
                 Id = newUid,
-                Title = m_title,
+                Title = mTitle,
                 Description = "NONE",
                 IsComplete = true,
                 CreatedAt = DateTime.Now
 
             };
             string json = JsonConvert.SerializeObject(taskModel, Formatting.Indented);
-            string full_json = json + "\n" + formattedJsonString;   
-            Console.WriteLine(full_json);
-            File.WriteAllText(m_filePath, full_json);
+            string fullJson = json + "\n" + formattedJsonString;   
+            Console.WriteLine(fullJson);
+            File.WriteAllText(_mFilePath, fullJson);
         }
     }
 }
