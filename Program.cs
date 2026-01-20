ef8e95d0ddd1453af3349810396f48c71c1d7219
RoeRL <bimo.17168@gmail.com>
2026-01-20 22:40:22 +0700

    Adding loopable program

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace TaskList
     {
         static void Main(string[] args)
         {
+            bool running = true;
             Console.Clear();
-            // Create an instance of the other class
             TaskService TS = new TaskService();
             var welcomeFiglet = new FigletText("Welcome To Task CLI")
                 {
@@ -18,31 +18,37 @@ namespace TaskList
                 }
                 .Color(Color.Blue);
             AnsiConsole.Write(welcomeFiglet);
-            
-            var features = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("Choose your [green]Option[/]")
-                    .AddChoices("Read Task", "Add Task", "Update", "Delete"));
-
-            switch (features)
+            while (running)
             {
-                case "Read Task":
-                    AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
-                    TS.ReadTask();
-                    break;
-                case "Add Task":
-                    AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
-                    TS.AddTask();
-                    break;
-                case "Update":
-                    AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
-                    break;
-                case "Delete":
-                    AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
-                    break;
-                default:
-                    AnsiConsole.MarkupLine("Not Found");
-                    break;
+                var features = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Choose your [green]Option[/]")
+                        .AddChoices("Read Task", "Add Task", "Update", "Delete", "Exit"));
+                switch (features)
+                {
+                    case "Read Task":
+                        AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
+                        TS.ReadTask();
+                        AnsiConsole.MarkupLine("[grey]Press any key to continue...[/]");
+                        Console.ReadKey(true);
+                        break;
+                    case "Add Task":
+                        AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
+                        TS.AddTask();
+                        break;
+                    case "Update":
+                        AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
+                        break;
+                    case "Delete":
+                        AnsiConsole.MarkupLine($"You Selected: [green]{features}[/]");
+                        break;
+                    case "Exit":
+                        running = false;
+                        break;
+                    default:
+                        AnsiConsole.MarkupLine("Not Found");
+                        break;
+                }
             }
         }
     }
